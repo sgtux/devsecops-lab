55594f447399600abac02d9cab0e5ac6ddc532a0
Gabriel <sgtux@protonmail.com>
2021-03-08 12:29:39 -0300

    Dotnet Site - Perfil page

---
+++ site-dotnet/files/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fakebook.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
