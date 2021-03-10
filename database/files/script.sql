47dec9e4f10ebf4ad7f3093f8478f52927a563e8
Gabriel <sgtux@protonmail.com>
2021-03-09 21:37:24 -0300

    Update database script

--- database/files/script.sql
+++ database/files/script.sql
@@ -11,10 +11,12 @@ CREATE TABLE "User" (
 
 CREATE TABLE "Post" (
   Id SERIAL,
-  Description VARCHAR(200),
+  Text VARCHAR(200) NOT NULL,
   Photo VARCHAR(200),
-	UserId INT,
-  CreatedAt TIMESTAMP,
+	UserId INT NOT NULL,
+  AccessType INT NOT NULL,
+  CreatedAt TIMESTAMP NOT NULL,
+  UpdatedAt TIMESTAMP,
   PRIMARY KEY (Id),
   CONSTRAINT FK_Post_User FOREIGN KEY (UserId) REFERENCES "User"(Id)
 );
@@ -45,7 +47,20 @@ CREATE TABLE "PostCommentLike" (
   CONSTRAINT FK_PostCommentLike_PostComment FOREIGN KEY (PostCommentId) REFERENCES "User"(Id)
 );
 
+CREATE TABLE "UserFriend" (
+	UserId INT,
+  FriendId INT,
+  CONSTRAINT FK_UserFriend_User FOREIGN KEY (UserId) REFERENCES "User"(Id),
+  CONSTRAINT FK_UserFriend_Friend FOREIGN KEY (FriendId) REFERENCES "User"(Id),
+  PRIMARY KEY (UserId, FriendId)
+);
+
 INSERT INTO "User" (Nick, Email, Password, CreatedAt)
   VALUES
-('Naruto Uzumaki', 'narutouzumaki@internet.lab', '6a8047cda9f32487f243a3c3569bb84b', to_timestamp('2020-06-22 17:50:10','YYYY-MM-DD HH24:MI:SS')),
-('Shikamaru Nara', 'shikamarunara@internet.lab', '6a8047cda9f32487f243a3c3569bb84b', to_timestamp('2020-08-13 20:33:23','YYYY-MM-DD HH24:MI:SS'));
+('Naruto Uzumaki', 'narutouzumaki@fakebook.lab', '6a8047cda9f32487f243a3c3569bb84b', to_timestamp('2020-06-22 17:50:10','YYYY-MM-DD HH24:MI:SS')),
+('Shikamaru Nara', 'shikamarunara@fakebook.lab', '6a8047cda9f32487f243a3c3569bb84b', to_timestamp('2020-08-13 20:33:23','YYYY-MM-DD HH24:MI:SS')),
+('Harry Potter', 'harrypotter@fakebook.lab', '6a8047cda9f32487f243a3c3569bb84b', to_timestamp('2020-06-22 17:50:10','YYYY-MM-DD HH24:MI:SS')),
+('Hermione Granger', 'hermionegranger@fakebook.lab', '6a8047cda9f32487f243a3c3569bb84b', to_timestamp('2020-06-22 17:50:10','YYYY-MM-DD HH24:MI:SS')),
+('Rony Weasley', 'ronyweasley@fakebook.lab', '6a8047cda9f32487f243a3c3569bb84b', to_timestamp('2020-06-22 17:50:10','YYYY-MM-DD HH24:MI:SS')),
+('Sirius Black', 'siriusblack@fakebook.lab', '6a8047cda9f32487f243a3c3569bb84b', to_timestamp('2020-06-22 17:50:10','YYYY-MM-DD HH24:MI:SS')),
+('Lord Voldemort', 'lordvoldemort@fakebook.lab', '6a8047cda9f32487f243a3c3569bb84b', to_timestamp('2020-06-22 17:50:10','YYYY-MM-DD HH24:MI:SS'));
